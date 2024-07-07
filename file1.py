436f51e2005ecc44a2e8529828ba8526421e59cb
princesshavy <havilah757@gmail.com>
2024-07-07 17:37:06 +0100

    list length

---
+++ file1.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit(celsius):
+    return ((celsius * 9/5) + 32)
+
+celsius = 25
+# fahrenheit = celsius_to_fahrenheit(celsius)
+# print(f"{celsius}°C is {fahrenheit}°F")
+print(celsius_to_fahrenheit(34))
