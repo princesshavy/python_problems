82d633415360f38ef649fe95e1aab7fa3a01f130
princesshavy <havilah757@gmail.com>
2024-07-13 16:36:07 +0100

    list to string

---
+++ assignment_problem.py
@@ -0,0 +1,17 @@
+# def list_to_String(s):
+
+#     string = ""
+    
+
+#     for ele in s:
+#         string += ele
+
+#     return string
+
+# s = list(input ("Enter the list in a square bracket:"))
+# print(list_to_String(s))
+
+
+lst = ['H', 'e', 'l', 'l', 'o']
+string = ''.join(lst)
+print("String:", string)
